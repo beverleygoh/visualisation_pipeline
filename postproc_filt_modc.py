2da1a9cbde24552671de93e48721816f4704dbf2
beverleygoh <137620697+beverleygoh@users.noreply.github.com>
2023-06-29 09:37:42 +0300

    Update postproc_filt_modc.py

--- postproc_filt_modc.py
+++ postproc_filt_modc.py
@@ -224,7 +224,7 @@ if __name__ == "__main__":
     global tend, n, fromlist, node2index2, edge_filterdf, node_index, nodelist
 
     if len(argv) != 2:
-        print('usage: python3 metric_structural4-1-1.py [file name]')
+        print('usage: python3 postproc_filt_modc.py [file name]')
         exit()
 
     df = pd.read_csv(argv[1],delimiter = ';')
